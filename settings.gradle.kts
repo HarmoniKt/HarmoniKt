622db6df94b6e17d7def87b66645e35ea3cb4ca2
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-03-19 12:43:36 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.gradle.develocity") version "3.19.2"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.21"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+        publishing.onlyIf { it.buildResult.failures.isNotEmpty() }
+    }
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks()
+}
+
+rootProject.name = "Template-for-Kotlin-JVM-Projects"
