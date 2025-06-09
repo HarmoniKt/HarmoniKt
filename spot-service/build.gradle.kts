7837f40c502b1218d3b4dbff8655b254e4702ab7
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-06-09 20:26:30 +0200

    build: manage deps

--- spot-service/build.gradle.kts
+++ spot-service/build.gradle.kts
@@ -2,6 +2,4 @@ dependencies {
     implementation(rootProject.libs.bundles.ktor.server)
     implementation(project(":model"))
     implementation("io.ktor:ktor-server-content-negotiation:3.0.3")
-    implementation("io.ktor:ktor-serialization-gson:3.0.3")
-    implementation("io.ktor:ktor-server-content-negotiation:3.0.3")
 }
