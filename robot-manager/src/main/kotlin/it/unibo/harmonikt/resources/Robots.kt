dd6476a656ac2d4272c89bab600ae9f81db4e9bf
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-07-14 17:11:16 +0200

    feat: add robot structure

--- robot-manager/src/main/kotlin/it/unibo/harmonikt/resources/Robots.kt
+++ robot-manager/src/main/kotlin/it/unibo/harmonikt/resources/Robots.kt
@@ -1,9 +1,32 @@
 package it.unibo.harmonikt.resources
 
 import io.ktor.resources.Resource
+import it.unibo.harmonikt.model.RobotId
 
 /**
  * Represents the Robots resource in the Robot Manager service.
+ * This resource is used to manage robots in the system.
  */
 @Resource("/robots")
-class Robots
+class Robots {
+    /**
+     * Represents a specific robot identified by its ID.
+     *
+     * This nested resource allows for operations on a specific robot
+     * by its unique identifier.
+     * @param parent The parent Robots resource.
+     * @param robotId The unique identifier of the robot.
+     */
+    @Resource("{robotId}")
+    class Id(val parent: Robots, val robotId: RobotId) {
+        /**
+         * Represents the actions resource for a specific robot.
+         *
+         * This nested resource allows for operations on actions
+         * associated with a specific robot.
+         * @param parent The parent Id resource.
+         */
+        @Resource("actions")
+        class Actions(val parent: Id)
+    }
+}
