4c05bada890e1e3fd2efee886cc5a11b1db78f29
davidedomini <davide.domini@unibo.it>
2025-07-15 17:36:24 +0200

    feat: setup service skeleton

--- spot-service-py/app/main.py
+++ spot-service-py/app/main.py
@@ -1,2 +1,8 @@
-if __name__ == '__main__':
-    pass
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+
+app = FastAPI(title="Spot Service", version="1.0.0")
+
+@app.get('/robots')
+async def get_robots():
+    return {'msg': 'It works!'}
