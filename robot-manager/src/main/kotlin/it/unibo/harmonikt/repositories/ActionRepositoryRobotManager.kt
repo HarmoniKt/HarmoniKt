21baf81fd7f299bb2b348868be655cb0f7f2ef18
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-07-31 18:45:24 +0200

    feat: add action main methods

--- robot-manager/src/main/kotlin/it/unibo/harmonikt/repositories/ActionRepositoryRobotManager.kt
+++ robot-manager/src/main/kotlin/it/unibo/harmonikt/repositories/ActionRepositoryRobotManager.kt
@@ -1,8 +1,14 @@
 package it.unibo.harmonikt.repositories
 
+import it.unibo.harmonikt.model.Action
+import it.unibo.harmonikt.model.RobotId
 import it.unibo.harmonikt.repository.ActionRepository
 
 /**
  * A repository for managing actions in the Robot Manager service.
  */
-class ActionRepositoryRobotManager : ActionRepository
+class ActionRepositoryRobotManager : ActionRepository {
+    override fun queueAction(robotId: RobotId, action: Action): Boolean {
+        TODO("Not yet implemented")
+    }
+}
