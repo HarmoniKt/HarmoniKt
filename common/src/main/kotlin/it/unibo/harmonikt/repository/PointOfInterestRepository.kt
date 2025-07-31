6f9bc520a6612585c20396b0637f3f1dba75963f
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-07-31 17:42:40 +0200

    feat: add POIs methods

--- common/src/main/kotlin/it/unibo/harmonikt/repository/PointOfInterestRepository.kt
+++ common/src/main/kotlin/it/unibo/harmonikt/repository/PointOfInterestRepository.kt
@@ -1,5 +1,8 @@
 package it.unibo.harmonikt.repository
 
+import it.unibo.harmonikt.model.PointOfInterest
+import kotlin.uuid.Uuid
+
 /**
  * Repository interface for managing points of interest in the system.
  * This interface defines the contract for storing, retrieving, and managing
@@ -8,4 +11,14 @@ package it.unibo.harmonikt.repository
  * Currently, this is a marker interface with no methods defined.
  * Implementations will provide specific functionality for managing points of interest.
  */
-interface PointOfInterestRepository
+interface PointOfInterestRepository {
+    fun getPointsOfInterest(): List<PointOfInterest>
+
+    fun registerPointOfInterest(name: String, latitude: Float, longitude: Float): Boolean
+
+    fun deletePointOfInterest(id: Uuid): Boolean
+
+    fun associateMarker(poiId: Uuid, markerId: Uuid): Boolean
+
+    fun dissociateMarker(poiId: Uuid, markerId: Uuid): Boolean
+}
--- robot-manager/src/main/kotlin/it/unibo/harmonikt/repositories/PointOfInterestRepositoryRobotManager.kt
+++ robot-manager/src/main/kotlin/it/unibo/harmonikt/repositories/PointOfInterestRepositoryRobotManager.kt
@@ -1,10 +1,36 @@
 package it.unibo.harmonikt.repositories
 
+import it.unibo.harmonikt.model.PointOfInterest
 import it.unibo.harmonikt.repository.PointOfInterestRepository
+import kotlin.uuid.Uuid
 
 /**
  * A repository for managing points of interest in the Robot Manager service.
  * This class implements the PointOfInterestRepository interface, providing methods
  * to interact with points of interest data.
  */
-class PointOfInterestRepositoryRobotManager : PointOfInterestRepository
+class PointOfInterestRepositoryRobotManager : PointOfInterestRepository {
+    override fun getPointsOfInterest(): List<PointOfInterest> {
+        TODO("Not yet implemented")
+    }
+
+    override fun registerPointOfInterest(
+        name: String,
+        latitude: Float,
+        longitude: Float
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun deletePointOfInterest(id: Uuid): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun associateMarker(poiId: Uuid, markerId: Uuid): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun dissociateMarker(poiId: Uuid, markerId: Uuid): Boolean {
+        TODO("Not yet implemented")
+    }
+}
