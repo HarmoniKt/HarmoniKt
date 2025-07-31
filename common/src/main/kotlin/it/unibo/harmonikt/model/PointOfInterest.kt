c3f78040e19cb31a016ca5397256b03dba82e59c
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-07-31 17:43:37 +0200

    feat: add more info about POI

--- common/src/main/kotlin/it/unibo/harmonikt/model/PointOfInterest.kt
+++ common/src/main/kotlin/it/unibo/harmonikt/model/PointOfInterest.kt
@@ -1,6 +1,7 @@
 package it.unibo.harmonikt.model
 
 import kotlinx.serialization.Serializable
+import kotlin.uuid.Uuid
 
 /**
  * Represents a location or area of interest in the environment.
@@ -8,6 +9,29 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 data class PointOfInterest(
+    /**
+     * Unique identifier for the point of interest.
+     */
+    val id: Uuid,
+    
+    /**
+     * Canonical name of the point of interest.
+     * This name is used to identify the point in a human-readable format.
+     */
+    val name: String,
+
+    /**
+     * Latitude of the point of interest.
+     * This is a floating-point value representing the geographical latitude.
+     */
+    val latitude: Float,
+
+    /**
+     * Longitude of the point of interest.
+     * This is a floating-point value representing the geographical longitude.
+     */
+    val longitude: Float,
+
     /**
      * The list of markers associated with this point of interest.
      * Different robot types may use different marker types to identify the same location.
