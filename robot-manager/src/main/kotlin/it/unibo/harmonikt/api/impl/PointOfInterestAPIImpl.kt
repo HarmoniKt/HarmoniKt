f243a8490ac795c03b07ec6ba1d1695e0d5a7a26
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-08-01 17:04:55 +0200

    feat: add POI API implementation base

---
+++ robot-manager/src/main/kotlin/it/unibo/harmonikt/api/impl/PointOfInterestAPIImpl.kt
@@ -0,0 +1,25 @@
+package it.unibo.harmonikt.api.impl
+
+import arrow.core.Either
+import it.unibo.harmonikt.api.PointOfInterestAPI
+import it.unibo.harmonikt.api.PointOfInterestAPIError
+import it.unibo.harmonikt.model.PointOfInterest
+
+class PointOfInterestAPIImpl : PointOfInterestAPI {
+    override suspend fun getPointOfInterest(id: String): Either<PointOfInterestAPIError, PointOfInterest> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun registerPointOfInterest(poi: PointOfInterest): Either<PointOfInterestAPIError, PointOfInterest> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deletePointOfInterest(id: String): Either<PointOfInterestAPIError, Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllPointsOfInterest(): Either<PointOfInterestAPIError, List<PointOfInterest>> {
+        TODO("Not yet implemented")
+    }
+
+}
