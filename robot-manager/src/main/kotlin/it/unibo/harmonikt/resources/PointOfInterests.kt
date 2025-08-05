54f630ef3061e67b1cd3dd0564b6d68e2789030e
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2025-08-05 17:11:09 +0200

    refactor: nest Marker resource into POI

--- robot-manager/src/main/kotlin/it/unibo/harmonikt/resources/PointOfInterests.kt
+++ robot-manager/src/main/kotlin/it/unibo/harmonikt/resources/PointOfInterests.kt
@@ -18,5 +18,36 @@ class PointOfInterests {
      * @property id identifier of the point of interest
      */
     @Resource("/{poiId}")
-    class Id(val parent: PointOfInterests, val id: Uuid)
+    class Id(val parent: PointOfInterests, val id: Uuid) {
+
+        /**
+         * Represents the markers associated with a specific point of interest.
+         *
+         * This nested resource allows for operations on markers related to a specific point of interest.
+         * @param parent The parent PointOfInterests.Id resource.
+         */
+        @Resource("/markers")
+        class Markers(val parent: PointOfInterests.Id) {
+            /**
+             * Represents a specific marker identified by its ID.
+             *
+             * This nested resource allows for operations on a specific marker
+             * by its unique identifier.
+             * @param parent The parent Marker resource.
+             * @param id The unique identifier of the marker.
+             */
+            @Resource("/{markerId}")
+            class Id(val parent: Markers, val id: Uuid) {
+                /**
+                 * Represents the position of a specific marker.
+                 *
+                 * This nested resource allows for operations on the position
+                 * of a specific marker by its unique identifier.
+                 * @param parent The parent Id resource.
+                 */
+                @Resource("/position")
+                class Position(val parent: Id)
+            }
+        }
+    }
 }
