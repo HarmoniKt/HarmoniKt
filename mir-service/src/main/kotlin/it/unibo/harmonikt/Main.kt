73b2c486c5b4c65ef72f9ffabfa531ef2cba1efe
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-09-09 12:47:39 +0200

    fix: remove duplicate plugin install

--- mir-service/src/main/kotlin/it/unibo/harmonikt/Main.kt
+++ mir-service/src/main/kotlin/it/unibo/harmonikt/Main.kt
@@ -32,7 +32,7 @@ fun main() {
     logger.info("Starting MIR service...")
     val server = embeddedServer(
         Netty,
-        port = 8090,
+        port = 8080,
         host = "0.0.0.0",
         module = Application::module,
     )
