da126323c8372a6971651c2838af20853fcf2ba2
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-09-09 14:05:53 +0200

    feat: mir robot handler

--- mir-service/src/main/kotlin/it/unibo/harmonikt/handlers/RobotHandlers.kt
+++ mir-service/src/main/kotlin/it/unibo/harmonikt/handlers/RobotHandlers.kt
@@ -1,11 +1,17 @@
 package it.unibo.harmonikt.handlers
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.receive
 import io.ktor.server.resources.delete
 import io.ktor.server.resources.get
 import io.ktor.server.resources.post
+import io.ktor.server.response.respond
 import io.ktor.server.routing.Routing
+import it.unibo.harmonikt.api.dto.RobotIdDTO
 import it.unibo.harmonikt.api.dto.RobotRegistrationDTO.MirRobotRegistrationDTO
+import it.unibo.harmonikt.api.dto.RobotStatusDTO
+import it.unibo.harmonikt.model.Robot
+import it.unibo.harmonikt.model.RobotType
 import it.unibo.harmonikt.repository.MirRobotRepository
 import it.unibo.harmonikt.resources.MirRobots
 
@@ -19,25 +25,53 @@ object RobotHandlers {
      * @param repository The MirRobotRepository instance to handle robot data.
      */
     fun Routing.setupRobotHandlers(repository: MirRobotRepository) {
+        // GET /robots - Retrieve all active robots
         get<MirRobots> {
-            repository.getRobots()
+            val robots = repository.getRobots()
+            call.respond(robots)
         }
 
+        // POST /robots - Add a new robot
         post<MirRobots> {
             val request = call.receive<MirRobotRegistrationDTO>()
-            repository.createRobot(request.canonicalName, request.apiToken, request.host)
+            val robotId = repository.createRobot(
+                request.canonicalName,
+                request.apiToken,
+                request.host,
+            )
+            if (robotId != null) {
+                call.respond(HttpStatusCode.Created, RobotIdDTO(robotId))
+            } else {
+                call.respond(HttpStatusCode.Conflict, "Robot ${request.canonicalName} not created.")
+            }
         }
 
+        // DELETE /robots/{robotId} - Remove a robot
         delete<MirRobots.Id> { robot ->
-            repository.deleteRobot(robot.id)
+            val done = repository.deleteRobot(robot.id)
+            if (done) {
+                call.respond(HttpStatusCode.OK, "Robot with id ${robot.id} deleted")
+            } else {
+                call.respond(HttpStatusCode.NotFound, "Delete failed for robot with id ${robot.id}, possibly not found")
+            }
         }
 
-        get<MirRobots.Id> { robot ->
-            repository.getRobotById(robot.id)
+        // GET /robots/{robotId} - Retrieve information about a robot
+        get<MirRobots.Id> { r ->
+            val robot: Robot? = repository.getRobotById(r.id)
+            if (robot != null) {
+                val dto = RobotStatusDTO(
+                    id = robot.id,
+                    canonicalName = robot.name,
+                    state = robot.currentState,
+                    batteryLevel = robot.batteryLevel,
+                    currentPosition = robot.currentPosition,
+                    type = RobotType.MIR,
+                )
+                call.respond(dto)
+            } else {
+                call.respond(HttpStatusCode.NotFound, "Robot with id ${r.id} not found")
+            }
         }
-
-//        post<MirRobots.Id.Move> { moveRequest ->
-//            call.respondText("Robot move command received for ID: ${moveRequest.parent.id}")
-//        }
     }
 }
